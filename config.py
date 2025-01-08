ad346ef9f5e534585411c4a3fbca6df36996b2bf
Ftbots <fndmydvc0077@gmail.com>
2025-01-09 02:36:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7880541615:AAFr0DBsUOMmOn5O6839FwzRY3RXfgShbB8")
-API_ID = int(os.environ.get("API_ID", "22287041"))
-API_HASH = os.environ.get("API_HASH", "c149386dcd58a40fa9fe60e632e161d4")
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://nr385708:bs6GdimYoAzmHbRF@cluster0.xtpwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-ADMINS = list(map(int, os.environ.get("ADMINS", "7597122443 7870803769 6104201545").split()))
-DUMP = int(os.environ.get("DUMP", "-1002478549413"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8153366664:AAFja6tp8BZZL51GzQmZmCfEc_H3mxo1Rbo")
+API_ID = int(os.environ.get("API_ID", "24994752"))
+API_HASH = os.environ.get("API_HASH", "1c9b10f27f4ab2811ed4f102cc005837")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://suryabhai991100:pPmTrc0DoyPsEcmn@cluster0.xpua4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ADMINS = list(map(int, os.environ.get("ADMINS", "6104201545 7870803769 6104201545").split()))
+DUMP = int(os.environ.get("DUMP", "-1002368509718"))
